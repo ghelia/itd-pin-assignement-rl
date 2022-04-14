c57872421f5d6e4ea8f6e526ca0b9687c249267e
Merlin Dugot <merlin.dugot@gmail.com>
2022-04-14 23:45:12 +0900

    reduce batch size and tweak hyperparameters

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import torch
 
 class Config:
 
-    learning_rate = 0.0003
+    learning_rate = 0.0001
     learning_rate_decay = 0.999
     n_epoch = 10000
-    n_episode = 10
-    batch_size = 1000
+    n_episode = 50
+    batch_size = 200
     paired_test_alpha = 0.02
 
     ntypes = 3
@@ -14,30 +14,30 @@ class Config:
     overlap_ratio = 0.25
     check_neighbors = True
 
-    items_emb_dim = 64
-    items_dense_hidden_dim = 128
+    items_emb_dim = 128
+    items_dense_hidden_dim = 512
     items_nheads = 8
-    items_nlayers = 2
+    items_nlayers = 3
 
-    items_query_dense_hidden_dim = 128
+    items_query_dense_hidden_dim = 512
     items_query_nheads = 8
 
-    glimpse_dense_hidden_dim = 128
+    glimpse_dense_hidden_dim = 512
     glimpse_nheads = 8
 
     compatibility_emb = 128
 
-    nodes_emb_dim = 64
-    nodes_dense_hidden_dim = 128
+    nodes_emb_dim = 128
+    nodes_dense_hidden_dim = 512
     nodes_nheads = 8
-    nodes_nlayers = 2
+    nodes_nlayers = 3
 
     dtype = torch.float32
-    if torch.cuda.is_available():
-        device = torch.device("cuda")
-    else:
-        device = torch.device("cpu")
-    device = torch.device("cpu")
+    # if torch.cuda.is_available():
+    #     device = torch.device("cuda")
+    # else:
+    #     device = torch.device("cpu")
+    device = torch.device("cuda")
 
     placed_flag_index = ntypes
     placement_offset = placed_flag_index + 1
