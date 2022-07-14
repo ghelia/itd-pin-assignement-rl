f18e89be8e1db60ad4b77435f0b9ac655f6cccf1
Merlin Dugot <merlin.dugot@ghelia.com>
2022-07-14 18:26:13 +0900

    backup sources when training

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+import shutil
 
 import torch
 
@@ -10,6 +11,7 @@ from pa.recorder import Recorder
 session = datetime.now().strftime("%m_%d_%Y, %H:%M:%S")
 if not os.path.exists(os.path.join("./saves", session)):
     os.makedirs(os.path.join("./saves", session))
+shutil.copytree("./pa", os.path.join("./saves", session, "sources"))
 recorder = Recorder(os.path.join("./logs", session))
 save_path = os.path.join("./saves", session)
 agent = Agent()
